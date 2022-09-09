4a370f7412dff97c728f311bf17b255ff16be7e5
Debojeet Jha <debojeetjha@gmail.com>
2022-09-09 20:47:57 +0530

    Add project scrool

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import IntroSection from './components/introSection/introSection';
 import Navbar from './components/navbar/navbar';
+import Projects from './components/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <IntroSection />
+      <Projects />
     </div>
   );
 }
